0067cedafebff5ac1ff8cd486b1bc28295d56b36
Shunya Matsuno <id.shma@gmail.com>
2015-08-09 20:39:51 +0900

    first commit

--- arduino.js
+++ arduino.js
@@ -3,12 +3,12 @@ var SerialPort = require('serialport').SerialPort;
 var serial = new SerialPort('/dev/tty.usbmodem1451',{
     baudRate: 9600,
     dataBits: 8,
-        parity: 'none',
-        stopBits: 1,
-        flowControl: false
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
 });
 
 socket.on('voicedata', function (data) {
-  console.log(data);
-  serial.write(data + "\n");
+    console.log(data);
+    serial.write(data + "\n");
 });
